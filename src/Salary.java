2101f69819dc5e1cde4ebefecec5614d9cd1ce37
arsalsid <arsal243@gmail.com>
2020-03-02 22:33:29 +0500

    Salary

---
+++ src/Salary.java
@@ -0,0 +1,11 @@
+public class Salary {
+    public static void main(String[] args) {
+
+        String a = "Arsalan";
+        String b = "Arsalan";
+
+        System.out.println(a==b);
+
+
+    }
+}
