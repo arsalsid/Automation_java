450fe45127da29dc4a4d4f206b1751280a70a3c6
arsalsid <arsal243@gmail.com>
2020-03-03 01:41:39 +0500

    Salary and distance

---
+++ src/Distance.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Distance {
+    public static void main(String[] args) {
+
+        int m = 1000;
+        double km;
+
+
+        Scanner d = new Scanner(System.in);
+        System.out.println("Enter your Kilometer :");
+
+        double Distance = d.nextDouble();
+        double Meter = km * m ;
+        
+        System.out.println("Meter :" +Meter);
+//
+//        System.out.println("Enter your Distance in Meter : ");
+//        System.out.println("Enter your Distance in feets : ");
+//        System.out.println("Enter your Distance in inches : ");
+//        System.out.println("Enter your Distance in centimeter: ");
+
+
+
+
+    }
+}
--- src/Salary.java
+++ src/Salary.java
@@ -1,11 +1,23 @@
+import  java.util.Scanner;
+
 public class Salary {
     public static void main(String[] args) {
 
-        String a = "Arsalan";
-        String b = "Arsalan";
+            //Get input from user
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your Basic Salary : ");
 
-        System.out.println(a==b);
+        //Convert String values into Double
+        double Basic_Salary = Double.parseDouble(s.nextLine());
 
+            //Calculate Allowances fo Basic Salary
+        System.out.println("Your Gross Salary is :");
+        double Dearness_Allowance = Basic_Salary * .40;
+        double Rent_Allowance = Basic_Salary * .20;
+
+            //Get Gross salary
+        double Gross_Salary = Basic_Salary + Dearness_Allowance  + Rent_Allowance;
+        System.out.println(Gross_Salary);
 
     }
 }
