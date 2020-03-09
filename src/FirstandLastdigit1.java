217446a63c45a02298bdd55e05e67335610325d0
Arsalan Siddique <ASiddique@dd.local>
2020-03-09 18:39:23 +0500

    First & last digits (switch)

--- src/FirstandLastdigit1.java
+++ src/FirstandLastdigit1.java
@@ -2,21 +2,36 @@ import java.util.Scanner;
 public class FirstandLastdigit1 {
     public static void main(String[] args) {
 
-        int number, first_digit , last_digit;
+//        int number, first_digit , last_digit;
+//
+//
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("Enter your wish number : ");
+//        int num = s.nextInt();
+//
+//        first_digit = num;
+//        while (first_digit >= 10)
+//        {
+//            first_digit = first_digit / 10;
+//        }
+//
+//        last_digit = num % 10;
+//
+//        System.out.println("\n Your first number is : " + num + "=" +" " +first_digit);
+//        System.out.println("\n Your Second number is : " + num + "=" +" " +last_digit);
 
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter your wish number : ");
-        int num = s.nextInt();
+        char grade = 'B';
+        int point;
 
-        first_digit = num;
-        while (first_digit >= 10)
-        {
-            first_digit = first_digit / 10;
+        switch (grade){
+            case 'A' : point = 4;
+            case 'B' : point = 3;
+            case 'C' : point = 2;
+            case 'D' : point = 1;
+            default :  point = 0;
+
+            System.out.println(point);
+
         }
-
-        last_digit = num % 10;
-
-        System.out.println("\n Your first number is : " + num + "=" +" " +first_digit);
-        System.out.println("\n Your Second number is : " + num + "=" +" " +last_digit);
     }
 }
